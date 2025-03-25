9fdf88009810d727595ba0d584a44127a51dfc47
bebyayu246 <beby.ayu.ft24@mail.umy.ac.id>
2025-03-25 15:51:06 +0700

    Fungsi Insertionsort

--- Contoh_Insertionsort.cpp
+++ Contoh_Insertionsort.cpp
@@ -31,3 +31,26 @@ else
     cin >> arr[i]; //menyimpan nilai data n ke dalam array arr
 }
 }
+
+void insertionsort()
+{ //Procedure insertionsort
+
+    int temp; //membuat variable data temporer atau menyimpan sementara
+    int j, i; // membuat variable j sebagai penanda
+
+    for (i = 1; i <= n - 1; i++)
+{ //step 1
+
+    temp = arr[i]; //step 2
+
+    j = i - 1; //step 3
+
+    while (j >= 0 && arr[j] > temp) //step 4
+{
+    arr[j + 1] = arr[j]; //step 4a
+    j--; //step 4b
+}
+
+    arr[j + 1] = temp; //step 5
+}
+}
