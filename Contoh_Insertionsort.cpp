8e833e7279529ad4ad6ae478c2b85924d710adf2
bebyayu246 <beby.ayu.ft24@mail.umy.ac.id>
2025-03-25 15:36:13 +0700

    Inputan & batasan max 20

--- Contoh_Insertionsort.cpp
+++ Contoh_Insertionsort.cpp
@@ -3,3 +3,22 @@ using namespace std;
 
 int arr[20]; //membuat Array dengan panjang data 20
 int n; //membuat variable inputan n
+
+void input()
+{ //procedure input
+    while (true)
+{
+    cout << "Masukkan Jumlah Data pada Array : "; //membuat inputan jumlah elemen array
+    cin >> n; //memanggil variable inputan n
+
+    if ( n <= 20)
+{ //membuat kondisi n tidak lebih dari 20
+    break;
+}
+else
+{
+    cout << "\nArray yang anda masukkan maksimal 20 elemen.\n"; // menampilkan pesan jika data lebih dari 20
+}
+}
+
+}
